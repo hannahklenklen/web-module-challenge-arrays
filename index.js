f23df8f096c29d477e7fc16413667a9e9017727f
Hannah Klenklen <hannahklenklen@Hannahs-iMac.hsd1.co.comcast.net>
2020-11-04 13:43:47 -0700

    working on Task 3

--- index.js
+++ index.js
@@ -45,9 +45,11 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
-}    
+function copy(){
+    let copyFlavors = [...originalFlavors];
+    return copyFlavors;
+}  
+copy();   
 
 
 
@@ -64,9 +66,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
-}
+function is31Flavors(){
+    if(originalFlavors.length=31){
+      return true;
+    }
+ }
+ is31Flavors();
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,9 +86,11 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(){
+   let newFlavor = [originalFlavors.unshift("Rainbow Sherbert")];
+   return newFlavor;
 }
+addFlavor();
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
